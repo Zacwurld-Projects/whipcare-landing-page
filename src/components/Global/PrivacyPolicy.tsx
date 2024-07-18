f8ab7f7d6dd4431c92ff4dd03d743adb9d661baa
CharlesChinedum <charleschinedum2@gmail.com>
2024-07-18 19:50:07 +0100

    update: terms and conditions

--- src/components/Global/PrivacyPolicy.tsx
+++ src/components/Global/PrivacyPolicy.tsx
@@ -5,7 +5,7 @@ import { privacyPolicy } from '@/utils/privacy';
 
 const PrivacyPolicy = () => {
     return (
-        <div className="privacy-policy mt-[2rem] px-7 xl:px-[6rem] 2xl:px-5">
+        <div className="mt-[2rem] px-7 xl:px-[6rem] 2xl:px-5">
             <h2 className='font-medium text-4xl mb-3'>Privacy Policy</h2>
             <ReactMarkdown remarkPlugins={[remarkGfm]} className='leading-loose'>
                 {privacyPolicy}
--- src/components/Global/TermsAndConditions.tsx
+++ src/components/Global/TermsAndConditions.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { privacyPolicy } from '@/utils/privacy';
+import { termsAndConditions } from '@/utils/termsandconditions';
+import { terms } from '@/utils/terms';
 
 const TermsAndConditions = () => {
-  return (
-    <div className="privacy-policy mt-[2rem]">
-    <h2 className='font-medium text-4xl mb-3'>Privacy Policy</h2>
-    <ReactMarkdown remarkPlugins={[remarkGfm]} className='leading-loose'>
-        {privacyPolicy}
-    </ReactMarkdown>
-</div>
-  )
+    return (
+        <div className="privacy-policy mt-[2rem] px-7 xl:px-[6rem] 2xl:px-5">
+            <h2 className='font-medium text-4xl mb-3'>TERMS AND CONDITIONS</h2>
+            <ReactMarkdown remarkPlugins={[remarkGfm]} className='leading-loose'>
+                {terms}
+            </ReactMarkdown>
+        </div>
+    )
 }
 
 export default TermsAndConditions
