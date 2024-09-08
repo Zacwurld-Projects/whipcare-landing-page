02992a5324758c35a09c55f1b42d3278eed8dc03
charleschinedum <charleschinedum2@gmail.com>
2024-09-08 22:25:30 +0000

    update: explore animation v1

--- src/components/Explore/Hero.tsx
+++ src/components/Explore/Hero.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import backgroundImage from "@/assets/images/explore/background-image1.png"
 import Navbar from '../Hero/Navbar'
 
+import { motion } from 'framer-motion'
+import { fadeIn } from '@/variants'
+
 const Hero = () => {
     return (
         <section className='h-[40dvh] xl:h-[100dvh] mb-5 xl:mb-0'>
@@ -16,7 +19,12 @@ const Hero = () => {
                         hamburgerColor='text-white'
                     />
                     <div className='h-1/4 w-full flex items-center justify-center'>
-                        <h2 className='text-white text-xl xl:text-4xl font-semibold text-center'>Explore <br className='xl:hidden' /> Whipcare</h2>
+                        <motion.h2
+                            variants={fadeIn("up", 0.2)}
+                            initial="hidden"
+                            whileInView={"show"}
+                            viewport={{ once: false, amount: 0.5 }}
+                            className='text-white text-xl xl:text-4xl font-semibold text-center'>Explore <br className='xl:hidden' /> Whipcare</motion.h2>
                     </div>
                 </div>
 
